0a39f6bab3ae37f8f3946511c388235c2d8ba528
Daniel Cruz <danielcruzp@unitec.edu>
2026-04-26 18:56:25 -0600

    Agregada funcion para resolver parentesis

--- script.js
+++ script.js
@@ -36,5 +36,31 @@ function calculate() {
 
 function evaluate(expr) {
   // Logica de cada operacion aqui
+  expr = resolverParentesis(expr);
+  expr = resolverSuma(expr);
   return expr;
 }
+
+function resolverParentesis(expr) {
+  while (expr.includes("(")) {
+    const inicio = expr.lastIndexOf("(");
+    const fin = expr.indexOf(")", inicio);
+    const subExpresion = expr.substring(inicio + 1, fin);
+    console.log("Subexpresión:", subExpresion);
+    expr = expr.substring(0, inicio) + evaluate(subExpresion) + expr.substring(fin + 1);
+  }
+  return expr;
+}
+
+function resolverSuma(expr) {
+  expr = String(expr);
+
+  while (expr.includes("+")) {
+    const index = expr.indexOf("+");
+    const left = expr.substring(0, index);
+    const right = expr.substring(index + 1);
+    expr = String(parseFloat(left) + parseFloat(right));
+  }
+  return expr;
+}
+
