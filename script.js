80926156c693760c683e9ec138b9e7478a3ae723
Daniel Cruz <danielcruzp@unitec.edu>
2026-04-26 18:33:15 -0600

    Agregada function calculate y base de evaluate, conectada calculate al boton "="

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
+function appendToDisplay(input) {
+  const display = document.getElementById("display");
 
-function appendToDisplay(input)  {
-    const display = document.getElementById('display');
-
-    if(display.innerText =='0') {
-        display.innerText = input;
-    } else {
-        display.innerText += input;
-    }
-
-};
-
-function clearDisplay() {
-    const display = document.getElementById('display');
-    display.innerText = '0';
-};
-
-function clearOne(){
-    const display = document.getElementById('display');
-    if(display.innerText.length > 1) {
-        display.innerText = display.innerText.slice(0, -1);
-    } else {
-        display.innerText = '0';
-    }
+  if (display.innerText == "0") {
+    display.innerText = input;
+  } else {
+    display.innerText += input;
+  }
 }
 
+function clearDisplay() {
+  const display = document.getElementById("display");
+  display.innerText = "0";
+}
 
+function clearOne() {
+  const display = document.getElementById("display");
+  if (display.innerText.length > 1) {
+    display.innerText = display.innerText.slice(0, -1);
+  } else {
+    display.innerText = "0";
+  }
+}
 
+// Logica
+
+function calculate() {
+  const display = document.getElementById("display");
+  try {
+    const result = evaluate(display.innerText);
+    display.innerText = result;
+  } catch (e) {
+    display.innerText = "Error";
+  }
+}
+
+function evaluate(expr) {
+  // Logica de cada operacion aqui
+  return expr;
+}
